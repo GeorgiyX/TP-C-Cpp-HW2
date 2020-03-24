ce08b0049e06999fa2150caed4c6967e4a607d06
Georgiy <@GeorgiyX>
2020-03-24 20:46:17 +0300

    add: gtest fixes

--- project/stress_test/src/utils.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by adminu on 24.03.2020.
-//
-
--- project/tests/src/base_tests.cpp
+++ project/tests/src/base_tests.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <dlfcn.h>
+#include "utils.h"
 
 extern "C" {
 #include "sequence_search.h"
@@ -10,12 +10,6 @@ TEST(BASE_TESTS, STATIC_LIB_TST) {
     ASSERT_EQ(hello(), 1);
 }
 
-TEST(BASE_TESTS, DYNAMIC_LIB_TST) {
-    void *library;
-    int (*hello_so)(void);
-    library = dlopen("./../../libsequence_search_dynamic.so", RTLD_LAZY);
-    if (!library) { FAIL(); }
-    *(void **) (&hello_so) = dlsym(library, "hello");  // or reinterpret_cast
+TEST_F(SequenceSearchDynamic, DYNAMIC_LIB_TST) {
     ASSERT_EQ(hello_so(), 1);
-    dlclose(library);
 }
